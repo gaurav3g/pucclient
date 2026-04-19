63b635c26d703bbcf37887f31ef49e3d99ec2981
Gaurav Gupta <gauravgupta312@Gauravs-MacBook-Air.local>
2026-04-20 01:06:32 +0530

    multi tenant deployment

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/stage',
     createProxyMiddleware({
-      target: 'https://xjoexzabe3.execute-api.ap-south-1.amazonaws.com',
+      target: `https://${process.env.REACT_APP_API_GATEWAY_ID || 'xjoexzabe3'}.execute-api.ap-south-1.amazonaws.com`,
       changeOrigin: true,
       secure: true,
     })
--- src/utils/UserPool.js
+++ src/utils/UserPool.js
@@ -1,8 +1,8 @@
 import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 const poolData = {
-    UserPoolId: "ap-south-1_UGS66VEyd",
-    ClientId: "5vhhid5ak9k9ul7uqg42clfehv",
+    UserPoolId: process.env.REACT_APP_USER_POOL_ID || "ap-south-1_UGS66VEyd",
+    ClientId: process.env.REACT_APP_CLIENT_ID || "5vhhid5ak9k9ul7uqg42clfehv",
 }
 
 export default new CognitoUserPool(poolData);
